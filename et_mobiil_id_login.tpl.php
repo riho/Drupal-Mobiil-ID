1fd272ad9908074894b2ed4cdc70b103789e8f22
Hannes Kirsman <hannes@mekaia.ee>
2012-08-17 15:34:56 +0300

    Add HTML to template

---
+++ et_mobiil_id_login.tpl.php
@@ -0,0 +1,8 @@
+<div class="mobiil-id-instructions"><?php print $instructions; ?></div>
+  <div class="mobiil-id-status-wrapper">
+    <div class="mobiil-id-verification-code"><?php print $verification_code; ?></div>
+    <div class="mobiil-id-outstanding-transaction">
+      <div class="mobiil-id-progress-bar"></div>
+    </div>
+    <div class="mobiil-id-result"></div>
+  </div>
